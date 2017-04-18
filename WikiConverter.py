7069594e8d995badd9f64ec39364d36f7a396f2e
renzokuken <monipute@renzokuken.eu>
2017-04-18 13:24:38 +0200

    reduce queue size

--- WikiConverter.py
+++ WikiConverter.py
@@ -27,7 +27,7 @@ class WikiDoStuff(object):
 
         self.extraction_status = Value('f', 0.0)
 
-        self.extraction_queue = Queue(maxsize=100000)
+        self.extraction_queue = Queue(maxsize=1000)
         self.writing_queue = Queue()
 
         nb_workers = cpu_count()
@@ -61,7 +61,8 @@ class WikiDoStuff(object):
                 extraction_status = u'%.02f%%'%status.value
                 if status.value > 99.89:
                     extraction_status = u'Complete'
-                msg_str = u'\rExtraction: {0:s} | Compressing {1:s}\r'.format(extraction_status, next(spinner_chars))
+                msg_str = u'\rExtraction: {0:s} | Compressing {1:s} | Results Queue Size: {2:d}\r'.format(
+                        extraction_status, next(spinner_chars), ctrl_queue.qsize())
                 sys.stdout.write(msg_str)
                 sys.stdout.flush()
             message_json = in_queue.get()
